2b920685ae1966956c8a595cff8f24db75e78a4d
Luke Ghenco <lukeghenco@gmail.com>
2017-02-13 21:30:38 -0500

    fix syntax and switch to chai for ActorsTests.js

--- test/ActorsTest.js
+++ test/ActorsTest.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import sinon from 'sinon'
-import { mount, shallow } from 'enzyme'
-import pry from 'pryjs';
+import { expect } from 'chai';
+import { shallow } from 'enzyme'
 
 // components
 import Actors from '../src/components/Actors'
@@ -12,30 +12,30 @@ import { actors } from '../src/data'
 
 describe('<Actors />', () => {
 
-    it('should render one <h1 />, inside of a <div />', () => {
-        const wrapper = shallow(<Actors />)
-        expect(wrapper.children().first().type()).toBe('h1')
-    })
+  it('should render one <h1 />, inside of a <div />', () => {
+    const wrapper = shallow(<Actors />);
+    expect(wrapper.children().first().type()).to.equal('h1');
+  });
 
-    it("should render 'Actors Page' inside of the <h1 />", () => {
-        const wrapper = shallow(<Actors />)
-        expect(wrapper.children().first().text()).toContain('Actors Page')
-    })
+  it("should render 'Actors Page' inside of the <h1 />", () => {
+    const wrapper = shallow(<Actors />);
+    expect(wrapper.children().first().text()).to.contain('Actors Page');
+  });
 
-    it("should render a <div /> for each actor", () => {
-        const wrapper = shallow(<Actors />)
-        expect(wrapper.children().find('div').length).toBe(4)
-    })
+  it("should render a <div /> for each actor", () => {
+    const wrapper = shallow(<Actors />);
+    expect(wrapper.children().find('div').length).to.equal(4);
+  });
 
-    it("should render the right content for each actor with a className of 'actor'", () => {
-        const wrapper = mount(<Actors />);
-        const actorContainers = wrapper.children().find('div')
-        expect(actorContainers.length).toBe(4);
-        actorContainers.forEach((node, i) => {
-            expect(node.html()).toContain(actors[i].name)
-            actors[i].movies.forEach((movie) => {
-                expect(node.html()).toContain(movie)
-            })
-        })
-    })
-})
+  it("should render the right content for each actor with a className of 'actor'", () => {
+    const wrapper = shallow(<Actors />);
+    const actorContainers = wrapper.children().find('div');
+    expect(actorContainers.length).to.equal(4);
+    actorContainers.forEach((node, i) => {
+        expect(node.html()).to.contain(actors[i].name);
+        actors[i].movies.forEach((movie) => {
+            expect(node.html()).to.contain(movie);
+        });
+    });
+  });
+});
