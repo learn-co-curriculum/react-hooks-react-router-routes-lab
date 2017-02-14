3ec287d1d5987895f91cd73dc509ebd0f1f4f97a
Luke Ghenco <lukeghenco@gmail.com>
2017-02-13 21:26:38 -0500

    change Actors to be default exports

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { actors } from '../data'
+import React from 'react';
+import { actors } from '../data';
 
 const Actors = () => {
-    return (<div />)
-}
+  return (<div />);
+};
 
-module.exports = Actors
+export default Actors;
