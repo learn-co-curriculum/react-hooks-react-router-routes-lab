156b4d7ea790e76a14523462d9d8dd6afff7483d
Yechiel K <contact@yechiel.me>
2017-03-27 21:40:18 -0400

    Fixed typo in the lab

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 
 // routes
-import routes from './routes';
+import routes from './Routes';
 
 /**
-  TODO: move all Route tags to ./route.js
+  TODO: move all Route tags to ./Routes.js
 */
 render (
   <Router history={browserHistory}>
