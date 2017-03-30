5b1a4692a99f91179db310a18c7bbb5e1abe6682
Luke Ghenco <lukeghenco@gmail.com>
2017-03-30 16:42:33 -0400

    passing App component tests

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route
+} from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import Home from '../components/Home';
+import Actors from '../components/Actors';
+import Directors from '../components/Directors';
+import Movies from '../components/Movies';
+
 
 const App = (props) => {
   return (
-    <div className="app">
-    </div>
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/movies" component={Movies} />
+      </div>
+    </Router>
   );
 };
 
