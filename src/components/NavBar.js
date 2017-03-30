7f5c0790d47f9c94bd1cdd9d959c932d6b3d98d0
Luke Ghenco <lukeghenco@gmail.com>
2017-03-30 17:16:45 -0400

    passing new NavBar tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 };
 
 export default NavBar;
