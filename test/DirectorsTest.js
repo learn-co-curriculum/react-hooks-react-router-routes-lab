89ec06ebe3fffc69c32b238d39adecb56854dc2f
Luke Ghenco <lukeghenco@gmail.com>
2017-03-30 17:20:53 -0400

    fix directors test with passing code

--- test/DirectorsTest.js
+++ test/DirectorsTest.js
@@ -9,25 +9,26 @@ import Directors from '../src/components/Directors'
 // data
 import { directors } from '../src/data'
 
-describe('<Directors />', () => {
+describe('Directors', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Directors />);
+  })
 
   it('should render one <h1 /> first, inside of the <div />', () => {
-    const wrapper = shallow(<Directors />);
     expect(wrapper.children().first().type()).to.equal('h1');
   });
 
   it("should render 'Directors Page' inside of the <h1 />", () => {
-    const wrapper = shallow(<Directors />);
     expect(wrapper.children().first().text()).to.contain('Directors Page');
   });
 
   it("should render a <div /> for each director", () => {
-    const wrapper = shallow(<Directors />);
     expect(wrapper.children().find('div').length).to.equal(3);
   });
 
   it("should render the right content for each director", () => {
-    const wrapper = shallow(<Directors />);
     const directorContainers = wrapper.children().find('div');
     expect(directorContainers.length).to.equal(3);
     directorContainers.forEach((node, i) => {
