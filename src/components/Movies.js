0c226f71a32918375b6ca303a6bd5cedbfecf822
Luke Ghenco <lukeghenco@gmail.com>
2017-03-30 17:23:35 -0400

    update tests and solution for Movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+          <div key={index}>
+              <h3>Name: {movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              <ul>
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
