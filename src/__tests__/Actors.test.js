7782952b38b11f34021527c6481ea45f10896430
Matt Eva <matthew.eva@flatironschool.com>
2023-10-18 12:42:32 -0700

    refactored Actor and Director test to not use data.js file

--- src/__tests__/Actors.test.js
+++ src/__tests__/Actors.test.js
@@ -2,7 +2,29 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { RouterProvider, createMemoryRouter} from "react-router-dom";
 import routes from "../routes";
-import { actors } from "../data";
+
+const actors = [
+  {
+    name: "Benedict Cumberbatch",
+    movies: ["Doctor Strange", "The Imitation Game", "Black Mass"],
+  },
+  {
+    name: "Justin Timberlake",
+    movies: ["Trolls", "Friends with Benefits", "The Social Network"],
+  },
+  {
+    name: "Anna Kendrick",
+    movies: ["Pitch Perfect", "Into The Wood"],
+  },
+  {
+    name: "Tom Cruise",
+    movies: [
+      "Jack Reacher: Never Go Back",
+      "Mission Impossible 4",
+      "War of the Worlds",
+    ],
+  },
+];
 
 const router = createMemoryRouter(routes, {
   initialEntries: [`/actors`],
